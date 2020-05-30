3b59ea0b0874e711b0e5ff7e7bb809d6bc08411c
AjayKMehta <AjayMehta1@gmail.com>
2020-05-29 17:57:06 -0700

    Made several enhancements:
    1. Add ability to specify column names.
    2. Add pipeline support.
    3. Add ability to skip lines.
    4. Add verbose and debug output.

--- CSV/Import-DataTable.ps1
+++ CSV/Import-DataTable.ps1
@@ -3,13 +3,17 @@ function Import-DataTable {
     .SYNOPSIS
         Creates datatable from CSV file.
     .DESCRIPTION
-        Creates datatable from CSV file. Column names are taken from first row.
+        Creates datatable from CSV file. Column names are taken from first non-skipped row or can be provided.
     .PARAMETER LiteralPath
         This must be the path to a file. Wildcards are not supported.
     .PARAMETER Delimiter
         Specifies the delimiter that separates the property values in the CSV file. The default is a comma (,).
+    .PARAMETER Columns
+        Specifies an array of strings to use for column names.
     .PARAMETER Encoding
-        Specifies the encoding for the CSV file. Valid values are Unicode, UTF7, UTF8, ASCII, UTF32, BigEndianUnicode, Default, and OEM. The default is ASCII.
+        Specifies the encoding for the CSV file. Valid values are Unicode, UTF7, UTF8, ASCII, UTF32, BigEndianUnicode, Default, and OEM. The default is UTF8.
+    .PARAMETER Skip
+        Specifies the number of lines from the beginning of the file to skip.
     .PARAMETER DetectEncodingFromByteOrderMarks
         If specified, code detects the encoding by looking at the first three bytes of the stream.
         It automatically recognizes UTF-8, little-endian Unicode, and big-endian Unicode text if the file starts with the appropriate byte order marks.
@@ -18,46 +22,71 @@ function Import-DataTable {
         System.String
         You can pipe the path of a file to Import-DataTable.
     .NOTES
-        Adapted from code in URL in Link section.
+        Idea based on code in URL in Link section.
     .LINK
         https://blog.netnerds.net/2015/02/working-with-basic-net-datasets-in-powershell/
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Default')]
     [OutputType([System.Data.DataTable])]
     param
     (
         [ValidatePathExists(PathType = 'Leaf')]
-        [Parameter(Mandatory = $true)]
-        [Alias("PSPath", "File")]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Columns', ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Default', Position = 0, ValueFromPipelineByPropertyName = $true)]
+        [Alias('PSPath', 'File')]
         [string] $LiteralPath,
+
+        [ValidateNotNullOrEmpty()]
+        [Alias('Separator')]
+        [char] $Delimiter = ',',
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Columns')]
+        [string[]] $Columns,
 
         [System.Text.Encoding] $Encoding = [System.Text.Encoding]::Default,
 
-        [ValidateNotNullOrEmpty()]
-        [char] $Delimiter = ',',
+        [int] $Skip = 0,
 
         [switch] $DetectEncodingFromByteOrderMarks
     )
+    process {
+        [Regex] $splitBy = [regex]::new($Delimiter + '(?=(?:[^"]*\"[^"]*")*(?![^"]*"))')
+        [string] $file = Convert-Path $LiteralPath
 
-    [Regex] $splitBy = [regex]::new($Delimiter + '(?=(?:[^"]*\"[^"]*")*(?![^"]*"))')
-    [string] $file = Convert-Path $LiteralPath
+        $result = [System.Data.DataTable]::new()
+        $reader = [System.IO.StreamReader]::new($file, $Encoding, $DetectEncodingFromByteOrderMarks)
 
-    $result = [System.Data.DataTable]::new()
-    $reader = [System.IO.StreamReader]::new($file, $Encoding, $DetectEncodingFromByteOrderMarks)
-    [bool] $firstRow = $true
-
-    while ($line = $reader.ReadLine()) {
-        [object[]] $vals = $splitBy.Split($line)
-        if ($firstRow) {
-            foreach ($col in $vals) {
-                # Necessary to handle escaped column names!
-                $null = $result.Columns.Add(($col -replace '^"([^"]*)"$', '$1'))
+        [bool] $hasHeader = $false
+        if ($PSCmdlet.ParameterSetName -eq 'Columns') {
+            foreach ($col in $Columns) {
+                $null = $result.Columns.Add($col)
             }
-            $firstRow = $false
-        } else {
-            $null = $result.Rows.Add($vals)
+            $hasHeader = $true
         }
+
+        [int] $i = 0
+        while ($line = $reader.ReadLine()) {
+            $i++
+            if ($i -le $Skip) {
+                continue
+            }
+            Write-Verbose "Processing line $i of $file"
+            Write-Debug "line $i of $($file): $line"
+            [object[]] $vals = $splitBy.Split($line)
+            if (!$hasHeader) {
+                foreach ($col in $vals) {
+                    # Necessary to handle escaped column names!
+                    $null = $result.Columns.Add(($col -replace '^"([^"]*)"$', '$1'))
+                }
+                $hasHeader = $true
+            } else {
+                $null = $result.Rows.Add($vals)
+            }
+        }
+        $numLines = if ($Skip -gt $i) {0} else {$i - $Skip}
+        Write-Verbose "Read $numLines lines of $file"
+        # Add comma before so PowerShell doesn't convert to Object[]!
+        , $result
     }
-    # Add comma before so PowerShell doesn't convert to Object[]!
-    ,$result
 }
