3bf572ceeb05f6e52b5764274b5932c05543d10f
AjayKMehta <AjayMehta1@gmail.com>
2020-06-02 16:49:53 -0700

    Add Get-DuplicateModule.

---
+++ Modules/Get-DuplicateModule.ps1
@@ -0,0 +1,54 @@
+function Get-DuplicateModule {
+    <#
+    .SYNOPSIS
+        Get the modules for which there are multiple versions installed.
+    .DESCRIPTION
+        Get the modules for which there are multiple versions installed.
+    .PARAMETER All
+        If set, check modules in all folders in $env:PSModulePath. Else, it only looks in Current User
+        and All Users specific folders.
+    .EXAMPLE
+        Get-DuplicateModule
+    .EXAMPLE
+        Get-DuplicateModule -All
+    #>
+    param (
+        [switch] $All
+    )
+
+    $inUse = Get-Module | Select-Object Name, Version
+
+    filter test-used($Module) {
+        foreach ($inUseModule in $inUse) {
+            $result = $false
+            if ($Module.Name -eq $inUseModule.Name -and $Module.Version -eq $inUseModule.Version) {
+                $result = $true
+                break
+            }
+        }
+        $result
+    }
+
+    $prefix = if ($PSVersionTable.PSVersion.Major -lt 6) { 'Windows' }
+    $allowedPaths = "$Home\Documents\$($prefix)PowerShell\Modules", "$Env:ProgramFiles\$($prefix)PowerShell\Modules"
+
+    function is-ok([string] $Path) {
+        $result = $false
+        foreach ($modPath in $allowedPaths) {
+            if ($Path.StartsWith($modPath)) {
+                $result = $true
+                break
+            }
+        }
+        $result
+    }
+
+    Get-Module -ListAvailable |
+    Where-Object {$All -or (is-ok $_.Path)} |
+    Select-Object Name, Version, Path |
+    Sort-Object Name, Version -Descending |
+    Group-Object Name |
+    Where-Object Count -gt 1 |
+    Select-Object -ExpandProperty Group |
+    Select-Object Name, Version, @{L = 'InUse'; E = { test-used $_ } }, Path
+}
