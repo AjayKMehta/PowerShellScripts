27d8392cdff9206e3113be8704942300d0442eb1
AjayKMehta <AjayMehta1@gmail.com>
2020-06-03 16:24:50 -0700

    Added Get-FullPath.

---
+++ FileSystem/Get-FullPath.ps1
@@ -0,0 +1,17 @@
+filter Get-FullPath {
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Alias("PSPath")]
+        [string[]] $Path
+    )
+
+    foreach ($pathItem in $Path) {
+        if ($pathItem -eq "C:") {
+            $pathItem
+        } else {
+            # Can't use Convert-Path because it will fail if input path does not exist!
+            $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($pathItem)
+        }
+    }
+}
