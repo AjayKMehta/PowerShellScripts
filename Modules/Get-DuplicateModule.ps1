493d5dc56c19116b533fe8711a3f8b5dce118dda
AjayKMehta <AjayMehta1@gmail.com>
2020-06-08 21:32:41 -0700

    Refactored code.
    Added Name parameter.

--- Modules/Get-DuplicateModule.ps1
+++ Modules/Get-DuplicateModule.ps1
@@ -4,20 +4,29 @@ function Get-DuplicateModule {
         Get the modules for which there are multiple versions installed.
     .DESCRIPTION
         Get the modules for which there are multiple versions installed.
+    .PARAMETER Name
+        Specifies names or name patterns of modules that this cmdlet gets. Wildcard characters are permitted.
     .PARAMETER AllLocations
         If set, check modules in all folders in $env:PSModulePath. Else, it only looks in Current User
         and All Users specific folders.
     .EXAMPLE
         Get-DuplicateModule
     .EXAMPLE
+        Get-DuplicateModule P* -All
+    .EXAMPLE
         Get-DuplicateModule -All
     #>
     param (
+        [SupportsWildcards()]
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string[]] $Name,
+
         [Alias('All')]
         [switch] $AllLocations
     )
-
-    $inUse = Get-Module | Select-Object Name, Version
+    $params = $PSBoundParameters
+    $null = $params.Remove('AllLocations')
+    $inUse = Get-Module @params | Select-Object Name, Version
 
     filter test-used($Module) {
         foreach ($inUseModule in $inUse) {
@@ -30,10 +39,9 @@ function Get-DuplicateModule {
         $result
     }
 
-    $prefix = if ($PSVersionTable.PSVersion.Major -lt 6) { 'Windows' }
-    $allowedPaths = "$Home\Documents\$($prefix)PowerShell\Modules", "$Env:ProgramFiles\$($prefix)PowerShell\Modules"
-
     function test-ok([string] $Path) {
+        $prefix = if ($PSVersionTable.PSVersion.Major -lt 6) { 'Windows' }
+        $allowedPaths = "$Home\Documents\$($prefix)PowerShell\Modules", "$Env:ProgramFiles\$($prefix)PowerShell\Modules"
         $result = $false
         foreach ($modPath in $allowedPaths) {
             if ($Path.StartsWith($modPath)) {
@@ -44,7 +52,7 @@ function Get-DuplicateModule {
         $result
     }
 
-    Get-Module -ListAvailable |
+    Get-Module @params -ListAvailable |
     Where-Object {$AllLocations -or (test-ok $_.Path)} |
     Select-Object Name, Version, Path |
     Sort-Object Name, Version -Descending |
