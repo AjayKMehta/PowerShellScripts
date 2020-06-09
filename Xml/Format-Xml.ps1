be97d2cda97917396c5976a13beecdd891404916
AjayKMehta <AjayMehta1@gmail.com>
2020-06-08 21:34:01 -0700

    Clean up code and documentation comments.

--- Xml/Format-Xml.ps1
+++ Xml/Format-Xml.ps1
@@ -6,32 +6,10 @@ function Format-Xml {
         Formats XML file based on XmlWriterSettings supplied . If the input is a document, output is a string. If file, it will overwrite existing file with desired output.
     .PARAMETER LiteraPath
         Path to an XML file. Can take input from pipeline.
-    .PARAMETER XML
+    .PARAMETER Xml
         XML Document
-    .PARAMETER CheckCharacters
-        Indicates whether to check that characters are in the legal XML character set.
-        The default value is true.
-    .PARAMETER ConformanceLevel
-        Indicates whether to check that output is a well-formed XML 1.0 document or fragment.
-        Default value is [System.Xml.ConformanceLevel]::Document.
-    .PARAMETER DoNotEscapeUriAttributes
-        Indicates whether the XmlWriter does not escape URI attributes. The default value is false.
-    .PARAMETER Encoding
-        Specify the type of text encoding to use. Defaults to UTF8.
-    .PARAMETER Indent
-        Toggle indenting XML elements. Defaults to true.
-    .PARAMETER IndentChars
-        The character string to use when indenting. This is used when the Indent parameter is set to true.
-    .PARAMETER NamespaceHandling
-        Indicates whether to remove duplicate namespace declarations when writing XML content. By default, it does not.
-    .PARAMETER NewLineChars
-        The character string to use for line breaks.
-    .PARAMETER NewLineHandling
-        Indicates whether to normalize line breaks in the output.
-    .PARAMETER NewLineOnAttributes
-        Indicates whether to write attributes on a new line. Defaults to true.
-    .PARAMETER OmitXmlDeclaration
-        Indicates whether to omit an XML declaration. Defaults to false.
+    .PARAMETER Settings
+        Object ot type System.Xml.XmlWriterSettings.
     .EXAMPLE
         $settings = New-XmlWriterSettings -OmitXmlDeclaration
         Format-Xml -LiteralPath 'c:\git\test\test.csproj' -Settings $settings
@@ -47,7 +25,7 @@ function Format-Xml {
         [string] $LiteralPath,
 
         [Parameter(ParameterSetName = "XML", Mandatory = $true, Position = 0 )]
-        [XML] $XML,
+        [XML] $Xml,
 
         [Parameter(ParameterSetName = "LiteralPath", Mandatory = $true, Position = 0)]
         [Parameter(ParameterSetName = "XML", Mandatory = $true, Position = 1)]
@@ -56,14 +34,14 @@ function Format-Xml {
     )
     process {
         if ($PSCmdlet.ParameterSetName -eq 'LiteralPath' ) {
-            $xml = [xml](Get-Content $LiteralPath)
+            $Xml = [xml](Get-Content $LiteralPath)
             $xmlWriter = [Xml.XmlWriter]::Create($LiteralPath, $Settings)
-            $xml.Save($xmlWriter)
+            $Xml.Save($xmlWriter)
             $xmlWriter.Close()
         } else {
             [System.Text.StringBuilder] $sb = [System.Text.StringBuilder]::new()
             $xmlWriter = [Xml.XmlWriter]::Create($sb, $Settings)
-            $xml.Save($xmlWriter)
+            $Xml.Save($xmlWriter)
             $xmlWriter.Close()
             $sb.ToString()
         }
