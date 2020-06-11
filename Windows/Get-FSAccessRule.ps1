a95ac86e436abe1aa93dc4a1586fa37ed2ac681d
AjayKMehta <AjayMehta1@gmail.com>
2020-06-10 22:36:08 -0700

    Added Get-FSAccessRule.

---
+++ Windows/Get-FSAccessRule.ps1
@@ -0,0 +1,25 @@
+function Get-FSAccessRule {
+    <#
+    .SYNOPSIS
+        Returns FileSystemAccessRule objects for Path and Principal.
+    .DESCRIPTION
+        Returns FileSystemAccessRule objects for Path and Principal.
+    .PARAMETER Path
+        The path whose access you need to check.
+    .PARAMETER Principal
+        The principal for whom you need to check access.
+    .EXAMPLE
+        Get-FSAccessRule -Path "C:\temp"
+    #>
+    [CmdletBinding(PositionalBinding = $false)]
+    param
+    (
+        [SupportsWildcards()]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string] $Path,
+        [Parameter(Position = 0)]
+        [System.Security.Principal.WindowsPrincipal] $Principal = (Get-CurrentUser)
+    )
+
+    (Get-Acl -Path $Path -ea Stop).Access | Where-Object { $Principal.IsInRole($_.IdentityReference) }
+}
