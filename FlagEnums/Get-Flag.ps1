414104480b51658a5cbafb2c69b15d02199597c7
AjayKMehta <AjayMehta1@gmail.com>
2020-06-12 22:03:28 -0700

    Refactored and improved logic.

--- FlagEnums/Get-Flag.ps1
+++ FlagEnums/Get-Flag.ps1
@@ -1,59 +1,89 @@
 function Get-Flag {
     <#
     .SYNOPSIS
-        Returns all bit fields that are set for EnumValue.
+        Returns all bit fields that are set for input enum value.
     .DESCRIPTION
-        Returns all bit fields that are set for EnumValue.
+        Returns all bit fields that are set for input enum value.
     .PARAMETER EnumValue
-        The value for which you want to check bit fields. Type must be of flag enum or error will occur.
-    .PARAMETER ShowAll
-        If specified, will check all possible values of enum to see if set.
-        This may result in superfluous values if the enum has compound values defined (combination of 2 or more enum values).
-    .PARAMETER ExcludeCompound
-        If specified, compound values are excluded from the return value. Only applies when ShowAll is specified.
-    .EXAMPLE
-        [System.Security.AccessControl.FileSystemRights]$fsr = [System.Security.AccessControl.FileSystemRights]"DeleteSubdirectoriesAndFiles,Synchronize,Modify"
-        Get-Flag $fsr
-    .EXAMPLE
-        Get-Flag ([System.Security.AccessControl.FileSystemRights]::FullControl) -ShowAll
-    .EXAMPLE
-        [System.Text.RegularExpressions.RegexOptions]::None | Get-Flag
-    .EXAMPLE
-        Get-Flag ([System.Security.AccessControl.FileSystemRights]"Write") -ShowAll -ExcludeCompound
+        The value for which you wish to check bit fields. Type must be a flag enum (enum that has FlagsAttribute marked) or error will occur.
+    .PARAMETER Show
+        This can take one of 3 values: All, Compound or NoCompound.
+        If All, it will check all possible values of enum to see if they are set. This may result in superfluous values if the enum has compound values defined (combination of 2 or more enum values).
+
+        If Compound is specified, compound values are NOT broken into their constituent values.
+
+        If NoCompound is specified, compound values are excluded from the return value.
+    .NOTES
+        Due to a bug in the PowerShell cmdlet Sort-Object, this will return only 1 enum value when there is more than 1 value with same underlying numeric value (see last example for demonstration).
     .EXAMPLE
         ([System.Security.AccessControl.FileSystemRights]"Traverse"),[System.Security.AccessControl.FileSystemRights]::FullControl  | Get-Flag
+    .EXAMPLE
+        enum MyEnum {
+            None = 0
+            One = 1
+            Two = 2
+            Both  = 3
+        }
+
+        $x = [MyEnum]::Both
+        Get-Flag $x
+        Get-Flag $x -Show 'NoCompound'
+        Get-Flag $x -Show 'All'
+    .EXAMPLE
+
+    [Flags()]
+        enum MyEnum2 {
+            None = 0
+            One = 1
+            Uno = 1
+            Two = 2
+            Both = 3
+        }
+
+        $y = [MyEnum2]::Both
+        Get-Flag $y
+        # Due to a bug in Sort-Object, only 'Uno' is shown in the results below instead of both 'Uno' and 'One'.
+        Get-Flag $y -Show 'NoCompound'
+        Get-Flag $y -Show 'All'
     #>
+    [CmdletBinding(PositionalBinding = $false)]
     param
     (
         [ValidateScript( { Test-FlagEnum ($_.GetType()) })]
         [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
         $EnumValue,
-        [switch] $ShowAll,
-        [switch] $ExcludeCompound
-    )
-    begin {
-        # Compound values are not 2^n.
-        $filter = { $_ -ne 0 -and $EnumValue.HasFlag($_) -and (!$ExcludeCompound -or (Test-Integer ([Math]::Log($_, 2)))) }
-    }
 
+        [ValidateSet('All', 'Compound', 'NoCompound')]
+        [Parameter(Mandatory = $false)]
+        [string] $Show = 'Compound'
+    )
     process {
         [Type] $enumType = $EnumValue.GetType()
+        # When there are 2 enum fields having same numeric value, you get duplicates!
+        # For example, WriteData and CreateFiles for FileSystemRights.
+        $values = $enumType.GetEnumValues() | Sort-Object -Descending -Unique
 
         if ($EnumValue -eq 0) {
-            $EnumValue
+            if ($values[-1] -eq 0) {
+                $result = [System.Collections.ArrayList]::new(1)
+                $result.Add($EnumValue)
+            }
         } else {
-            $values = $enumType.GetEnumValues() | Select-Object -Unique
-            if ($ShowAll) {
-                $result = $values | Where-Object $filter
-            } else {
-                $result = @()
-                [int] $check = [int] $EnumValue
-                $eValue = $EnumValue
-                $values = $values | Sort-Object -Descending
-                foreach ($value in $values) {
-                    if ($eValue -eq $value -or $eValue.HasFlag($value)) {
-                        $result += $value
-                        $check -= [int] $value
+            [bool] $all = $Show -eq 'All'
+            [bool] $noCompound = $Show -eq 'NoCompound'
+
+            [int] $check = [int] $EnumValue
+            $eValue = $EnumValue
+            $result = [System.Collections.ArrayList]::new($values.Count)
+            foreach ($value in $values) {
+                [int] $val = [int] $value
+                if ($noCompound -and !(Test-Integer ([Math]::Log($val, 2)))) {
+                    continue;
+                }
+                if ($eValue.HasFlag($value)) {
+                    $null = $result.Add($value)
+                    if (!$all) {
+                        $check -= $val
                         if ($check -eq 0) {
                             break
                         }
@@ -61,7 +91,7 @@ function Get-Flag {
                     }
                 }
             }
-            $result
+            , $result
         }
     }
 }
