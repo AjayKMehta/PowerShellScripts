e3b593fe1b9340e5f168967045bdc39c90ca6715
AjayKMehta <AjayMehta1@gmail.com>
2020-06-16 18:30:28 -0700

    Refactored and added ExtraInfo parameter.

--- Modules/Get-DuplicateModule.ps1
+++ Modules/Get-DuplicateModule.ps1
@@ -1,3 +1,5 @@
+#Requires -Module PowerShellGet
+
 function Get-DuplicateModule {
     <#
     .SYNOPSIS
@@ -9,6 +11,10 @@ function Get-DuplicateModule {
     .PARAMETER AllLocations
         If set, check modules in all folders in $env:PSModulePath. Else, it only looks in Current User
         and All Users specific folders.
+    .PARAMETER ExtraInfo
+        If set, results will also include Installed, Scope and InUse properties.
+    .OUTPUTS
+        PSCustomObject[]
     .EXAMPLE
         Get-DuplicateModule
     .EXAMPLE
@@ -22,10 +28,12 @@ function Get-DuplicateModule {
         [string[]] $Name,
 
         [Alias('All')]
-        [switch] $AllLocations
+        [switch] $AllLocations,
+
+        [switch] $ExtraInfo
     )
     $params = $PSBoundParameters
-    $null = $params.Remove('AllLocations')
+    @('AllLocations', 'ExtraInfo').Foreach( { $null = $params.Remove($_) })
     $inUse = Get-Module @params | Select-Object Name, Version
 
     filter test-used($Module) {
@@ -52,12 +60,36 @@ function Get-DuplicateModule {
         $result
     }
 
-    Get-Module @params -ListAvailable |
-    Where-Object {$AllLocations -or (test-ok $_.Path)} |
+    $installed = Get-InstalledModule | Select-Object -ExpandProperty InstalledLocation
+    $allUsersFolder = Split-Path $profile.AllUsersAllHosts -Parent
+    $currentUserFolder = Split-Path $profile.CurrentUserAllHosts -Parent
+
+    function get-scope([string] $Path) {
+        if ($Path.StartsWith($allUsersFolder, $true, [CultureInfo]::InvariantCulture)) {
+            'AllUsers'
+        } elseif ($Path.StartsWith($currentUserFolder, $true, [CultureInfo]::InvariantCulture)) {
+            'CurrentUser'
+        } else {
+            'Other'
+        }
+    }
+
+
+    $result = Get-Module @params -ListAvailable |
+    Where-Object { $AllLocations -or (test-ok $_.Path) } |
     Select-Object Name, Version, Path |
     Sort-Object Name, Version -Descending |
     Group-Object Name |
     Where-Object Count -gt 1 |
     Select-Object -ExpandProperty Group |
-    Select-Object Name, Version, @{L = 'InUse'; E = { test-used $_ } }, Path
+    Select-Object Name, Version, Path
+
+    if ($ExtraInfo) {
+        $result |
+        Select-Object Name, Version, @{L = 'InUse'; E = { test-used $_ } },
+        @{L = 'Installed'; E = { (Split-Path $_.Path -Parent) -in $installed } },
+        @{L = 'Scope'; E = { get-scope $_.Path } }, Path
+    } else {
+        $result
+    }
 }
