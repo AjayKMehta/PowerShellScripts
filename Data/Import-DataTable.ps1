1a99fcbfa5862848cc28988db2858784df00f6e1
AjayKMehta <AjayMehta1@gmail.com>
2020-06-29 21:38:44 -0700

    :art: :sparkles: Add new parameters.
    Added UseCulture and TrimWhiteSpace switch parameters.
    Added CommentTokens.
    
    Renamed Columns parameter to Header.

--- Data/Import-DataTable.ps1
+++ Data/Import-DataTable.ps1
@@ -6,20 +6,6 @@ function Import-DataTable {
         Creates datatable from CSV file.
     .DESCRIPTION
         Creates datatable from CSV file. Column names are taken from first non-skipped row or can be provided.
-    .PARAMETER LiteralPath
-        This must be the path to a file. Wildcards are not supported.
-    .PARAMETER Delimiter
-        Specifies the delimiters that separate the property values in the CSV file. The default is a comma (,).
-    .PARAMETER Columns
-        Specifies an array of strings to use for column names.
-    .PARAMETER Encoding
-        Specifies the encoding for the CSV file. Valid values are Unicode, UTF7, UTF8, ASCII, UTF32, BigEndianUnicode, Default, and OEM. The default is UTF8.
-    .PARAMETER Skip
-        Specifies the number of lines from the beginning of the file to skip.
-    .PARAMETER DetectEncoding
-        If specified, code detects the encoding by looking at the first three bytes of the stream.
-        It automatically recognizes UTF-8, little-endian Unicode, and big-endian Unicode text if the file starts with the appropriate byte order marks.
-        Otherwise, the user-provided encoding is used.
     .INPUTS
         System.String
         You can pipe the path of a file to Import-DataTable.
@@ -33,25 +19,49 @@ function Import-DataTable {
     param
     (
         [ValidatePathExists(PathType = 'Leaf')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Columns', ValueFromPipelineByPropertyName = $true)]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Default', Position = 0, ValueFromPipelineByPropertyName = $true)]
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true)]
         [Alias('PSPath', 'File')]
+        # This must be the path to a file. Wildcards are not supported.
         [string] $LiteralPath,
 
         [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $false, Position = 1, ParameterSetName = 'Default')]
         [Alias('Separator')]
+        # Uses the list separator for the current culture as the item delimiter.
         [string[]] $Delimiter = ',',
 
-        [ValidateNotNullOrEmpty()]
-        [Parameter(Mandatory = $true, ParameterSetName = 'Columns')]
-        [string[]] $Columns,
+        [Parameter(ParameterSetName = 'Culture')]
+        # Uses the list separator for the current culture as the item delimiter.
+        [switch] $UseCulture,
 
+        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory = $false)]
+        # Specifies an array of strings to use for column names.
+        [string[]] $Header,
+
+        # Specifies the encoding for the CSV file. Valid values are Unicode, UTF7, UTF8, ASCII, UTF32, BigEndianUnicode, Default, and OEM. The default is UTF8.
         [System.Text.Encoding] $Encoding = [System.Text.Encoding]::Default,
 
+        # Specifies the number of *uncommented* lines from the beginning of the file to skip.
         [int] $Skip = 0,
 
-        [switch] $DetectEncoding
+        # If specified, code detects the encoding by looking at the first three bytes of the stream.
+        # It automatically recognizes UTF-8, little-endian Unicode, and big-endian Unicode text if the file starts with the appropriate byte order marks.
+        # Otherwise, the user-provided encoding is used.
+        [switch] $DetectEncoding,
+
+        [Parameter(Mandatory = $false)]
+        # Strings whose presence at beginning of lines indicates that line is a comment and should be ignored.
+        [string[]] $CommentTokens,
+
+        # If set, trim whitespace from values.
+        [switch] $TrimWhiteSpace
     )
+    begin {
+        if ($UseCulture) {
+            $Delimiter = [Cultureinfo]::CurrentCulture.TextInfo.ListSeparator
+        }
+    }
     process {
         [string] $file = Convert-Path $LiteralPath
 
@@ -59,10 +69,16 @@ function Import-DataTable {
         $reader = [TextFieldParser]::new($file, $Encoding, $DetectEncoding)
         $reader.TextFieldType = [FieldType]::Delimited
         $reader.Delimiters = $Delimiter
+        if ($CommentTokens) {
+            $reader.CommentTokens = $CommentTokens
+        }
+        if ($TrimWhiteSpace) {
+            $reader.TrimWhiteSpace = $TrimWhiteSpace
+        }
 
         [bool] $hasHeader = $false
-        if ($PSCmdlet.ParameterSetName -eq 'Columns') {
-            foreach ($col in $Columns) {
+        if ($Header) {
+            foreach ($col in $Header) {
                 $null = $result.Columns.Add($col)
             }
             $hasHeader = $true
@@ -81,8 +97,6 @@ function Import-DataTable {
                 if (!$hasHeader) {
                     foreach ($col in $vals) {
                         $null = $result.Columns.Add(($col))
-                        # Necessary to handle escaped column names!
-                        # $null = $result.Columns.Add(($col -replace '^"([^"]*)"$', '$1'))
                     }
                     $hasHeader = $true
                 } else {
