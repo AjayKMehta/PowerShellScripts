94f7b35329df7382b0eb70bb0957f99b52189d7c
AjayKMehta <AjayMehta1@gmail.com>
2020-06-30 21:36:31 -0700

    Add New-DynamicParameter.

---
+++ Metaprogramming/New-DynamicParameter.ps1
@@ -0,0 +1,29 @@
+using namespace System.Management.Automation
+
+Function New-DynamicParameter {
+    <#
+    .SYNOPSIS
+        Helper function for creating dynamic parameters.
+    .DESCRIPTION
+        Helper function for creating dynamic parameters.
+    #>
+    [CmdletBinding(PositionalBinding = $false)]
+    param (
+        # Name of the dynamic parameter
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $Name,
+
+        # Type for the dynamic parameter.
+        [Parameter(Mandatory = $true, Position = 1)]
+        [System.Type] $Type,
+
+        [Attribute[]] $Attribute
+    )
+
+    $AttributeCollection = [Collections.ObjectModel.Collection[System.Attribute]]::new()
+    foreach($attrib in $Attribute) {
+        $AttributeCollection.Add($attrib)
+    }
+
+    [RuntimeDefinedParameter]::new($Name, $Type, $AttributeCollection)
+}
