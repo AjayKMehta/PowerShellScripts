d0fdb7b4d3f6ed51945c09346c4a45e00a3180da
AjayKMehta <AjayMehta1@gmail.com>
2020-06-30 21:37:42 -0700

    Added dynamic parameter KeyField.
    Updated examples.
    Improved error message.

--- Data/Cast-Value.ps1
+++ Data/Cast-Value.ps1
@@ -1,4 +1,3 @@
-#TODO: Add dynamic parameter KeyField if UseDefaults not specified.
 function Cast-Value {
     <#
     .SYNOPSIS
@@ -28,6 +27,14 @@ function Cast-Value {
         # Uses $test from first example.
         $test | ConvertFrom-Csv | Cast-Value @{a=[int]} -ea SilentlyContinue -ErrorVariable e
         $e.Exception.Data # This has detailed error info
+    .EXAMPLE
+        $test = @"
+        id,a,b,c
+        A,1,2,3
+        B,a,12,4
+        "@
+        $test | ConvertFrom-Csv | Cast-Value -Map @{a=[int]} -ea SilentlyContinue -ErrorVariable e -KeyField id
+        $e.Exception.Data
     #>
     [Cmdletbinding(PositionalBinding = $false)]
     param
@@ -39,6 +46,18 @@ function Cast-Value {
         [hashtable] $PropertyTypeMap,
         [switch] $UseDefaults
     )
+
+    dynamicParam {
+        if (!$PSBoundParameters.ContainsKey('UseDefaults')) {
+            $paramAttrib = New-ParameterAttribute
+            $aliasAttrib = [Alias]::new(@('Id', 'IdField'))
+            $dynamicParam = New-DynamicParameter -Name 'KeyField' -Type ([string]) -Attribute $paramAttrib, $aliasAttrib
+
+            $paramDictionary = [System.Management.Automation.RuntimeDefinedParameterDictionary]::new()
+            $paramDictionary.Add('KeyField', $dynamicParam)
+            return $paramDictionary
+        }
+    }
 
     begin {
         [int] $ctr = 0
@@ -53,12 +72,21 @@ function Cast-Value {
                 if ($UseDefaults -or $parseInfo.IsParsed) {
                     $InputObject.$property = $parseInfo.Value
                 } else {
-                    [System.Exception] $exception = New-Object System.Exception
-                    @{"Id" = $ctr; "Value" = $origValue; "Field" = $property; "Type" = $type }.GetEnumerator() |
+                    $exception = [System.InvalidCastException]::new()
+                    @{"Record#" = $ctr; "Value" = $origValue; "Field" = $property; "Type" = $type }.GetEnumerator() |
                     ForEach-Object { $exception.data.Add($_.Key, $_.Value) }
 
                     if ($origValue -is [string]) { $origValue = "`"$origValue`"" }
-                    Write-Error -Message "Cannot convert value for $property ($origValue) to type $type." -Exception $exception
+                    [string] $KeyField = $null
+                    if ($PSBoundParameters.TryGetValue('KeyField', [ref] $KeyField)) {
+                        $keyValue = $Inputobject.$KeyField
+                        $exception.data.Add("Key ($KeyField)", $keyValue)
+                        if ($keyValue -is [string]) { $keyValue = "`"$keyValue`"" }
+                        $message = "Cannot convert value for record with {$keyField} = $keyValue and field {$property} = $origValue to type $type."
+                    } else {
+                        $message = "Cannot convert value for field {$property} = $origValue to type $type."
+                    }
+                    Write-Error -Message $message -Exception $exception
                 }
             }
         }
