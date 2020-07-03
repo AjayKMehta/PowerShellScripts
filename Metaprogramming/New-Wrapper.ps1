1b3f6aab45ff287e5c1ce0999d40cda371298c1b
AjayKMehta <AjayMehta1@gmail.com>
2020-07-02 22:37:52 -0700

    Made ChooseProperties a dynamic parameter.

--- Metaprogramming/New-Wrapper.ps1
+++ Metaprogramming/New-Wrapper.ps1
@@ -1,4 +1,5 @@
 using namespace System.Collections.Generic
+using namespace System.Management.Automation
 
 function New-Wrapper {
     <#
@@ -79,10 +80,6 @@ function New-Wrapper {
         [ValidateScript(
             {
                 if ($_.IsAbstract) { throw "Type cannot be abstract." }
-                # $UseDefaultConstructor will only be set if it precedes this parameter!
-                if ($UseDefaultConstructor -and !$_.GetConstructor([Type]::EmptyTypes)) {
-                    throw "No default constructor exists for $_"
-                }
                 return $true
             })
         ]
@@ -95,10 +92,6 @@ function New-Wrapper {
         # Name of new function to create.
         [string] $Name = "New-$($Type.Name)",
 
-        [Parameter(ParameterSetName = 'DefaultCons')]
-        # If specified, prompt the user to select properties.
-        [Switch] $ChooseProperties,
-
         # If specified, add OutputType attribute to created function.
         [switch] $AddOutputType,
 
@@ -108,109 +101,128 @@ function New-Wrapper {
         # If set, will set CmdletBinding for new function.
         [switch] $CmdletBinding
     )
+    dynamicparam {
+        if ($PSBoundParameters.ContainsKey('UseDefaultConstructor')) {
+            $paramAttrib = New-ParameterAttribute -ParameterSetName 'DefaultCons' -HelpMessage 'If specified, prompt the user to select properties.'
 
-    $resType = $Type.Tostring()
+            $dynamicParam = New-DynamicParameter -Name 'ChooseProperties' -Type ([SwitchParameter]) -Attribute $paramAttrib
 
-    if ($AddOutputType) {
-        $Attribs = "`r`n    [OutputType([$restype])]"
-    }
-    if ($CmdletBinding) {
-        $Attribs = "$Attribs`r`n    [CmdletBinding()]"
-    }
-
-    if ($PScmdlet.ParameterSetName -eq 'DefaultCons') {
-        $properties = $Type.GetProperties().Where( { $_.SetMethod -and $_.CanWrite }) | Select-Object Name, PropertyType
-
-        if ($ChooseProperties -and $properties) {
-            $properties = $properties | Out-GridView -PassThru -Title 'Select properties'
-            if (!$properties) {
-                throw "Operation canceled."
-            }
+            $paramDictionary = [RuntimeDefinedParameterDictionary]::new()
+            $paramDictionary.Add('ChooseProperties', $dynamicParam)
+            return $paramDictionary
         }
-        if ($properties.Count -gt 0) {
-            $params = foreach ($p in $properties) {
-                [string] $paramType = $p.PropertyType
-                $paramName = $p.Name.Substring(0, 1).ToUpper() + $p.Name.SubString(1)
-                if (!($NoSwitch) -and $p.PropertyType -eq [bool]) {
-                    $paramType = 'switch'
-                }
-                "        [$paramType] `$$($paramName)"
+    }
+
+    end {
+        $resType = $Type.Tostring()
+
+        if ($AddOutputType) {
+            $Attribs = "`r`n    [OutputType([$restype])]"
+        }
+        if ($CmdletBinding) {
+            $Attribs = "$Attribs`r`n    [CmdletBinding()]"
+        }
+
+        if ($PScmdlet.ParameterSetName -eq 'DefaultCons') {
+            if (!$Type.GetConstructor([Type]::EmptyTypes)) {
+                throw "No public default constructor exists for $_"
             }
-            $params = $params -join ",`r`n"
-            if ($CmdletBinding) {
-                $body = @"
+            $properties = $Type.GetProperties().Where( { $_.SetMethod -and $_.CanWrite }) |
+            Select-Object Name, PropertyType
+
+            if ($properties) {
+                $choose = $false
+                if ($PSBoundParameters.TryGetValue('ChooseProperties', [ref] $choose) -and $choose) {
+                    $properties = $properties | Out-GridView -PassThru -Title 'Select properties'
+                    if (!$properties) {
+                        throw "Operation canceled."
+                    }
+                }
+            }
+
+            if ($properties.Count -gt 0) {
+                $params = foreach ($p in $properties) {
+                    [string] $paramType = $p.PropertyType
+                    $paramName = $p.Name.Substring(0, 1).ToUpper() + $p.Name.SubString(1)
+                    if (!($NoSwitch) -and $p.PropertyType -eq [bool]) {
+                        $paramType = 'switch'
+                    }
+                    "        [$paramType] `$$($paramName)"
+                }
+                $params = $params -join ",`r`n"
+
+                if ($CmdletBinding) {
+                    $body = @"
     # Add similar logic for optional common parameters if necessary.
     foreach (`$p in [System.Management.Automation.Cmdlet]::CommonParameters) {
         `$null = `$PSBoundParameters.Remove(`$p)
     }
     New-Object $resType -Property `$PSBoundParameters
 "@
-            } else {
-                $body = "    New-Object $resType -Property `$PSBoundParameters"
-            }
-        } else {
-            $params = ''
-            $body = "    New-Object $resType"
-        }
-    } else {
-        $constructors = $Type.GetConstructors()
-
-        if ($constructors.Count -eq 0) {
-            throw "$($Type.Name) has no public instance constructors"
-        } elseif ($constructors.Count -eq 1) {
-            $cons = $constructors[0] |
-            Select-Object @{Name = 'Signature'; Expression = { $_.ToString() } },
-            @{Name = 'Params'; Expression = { $_.GetParameters() } }
-        } else {
-            $cons = $constructors |
-            Select-Object @{Name = 'Signature'; Expression = { $_.ToString() } },
-            @{Name = 'Params'; Expression = { $_.GetParameters() } } |
-            Out-GridView -PassThru
-            if (!$cons) {
-                throw "Operation canceled."
-            }
-        }
-
-        $numParams = $cons.Params.Count
-
-        if ($numParams -gt 0) {
-            $paramList = [List[string]]::new($numParams)
-            $valList = [List[string]]::new($numParams)
-
-            foreach ($p in $cons.Params) {
-                [string] $paramType = $p.ParameterType
-                $paramName = $p.Name.Substring(0, 1).ToUpper() + $p.Name.SubString(1)
-                $valList.Add("`$$ParamName")
-
-                [bool] $isBool = $p.ParameterType -eq [bool]
-                if (!($NoSwitch) -and $isBool) {
-                    $paramType = 'switch'
-                    $hasSwitch = $true
-                }
-                if (!$hasSwitch -and $p.HasDefaultValue) {
-                    $defValue = $p.DefaultValue
-                    if ($isBool) {
-                        $defValue = if ($defValue) { '$true' } else { '$false' }
-                    } elseif ($null -eq $defValue) {
-                        $defValue = '$null'
-                    }
-                    $paramList.Add("        [$paramType] `$$($ParamName) = $defValue")
                 } else {
-                    $paramList.Add("        [$paramType] `$$($ParamName)")
+                    $body = "    New-Object $resTy e -Property `$PSBoundParameters"
+                }
+            } else {
+                $params = ''
+                $body = "    New-Object $resType"
+            }
+        } else {
+            $constructors = $Type.GetConstructors()
+
+            if ($constructors.Count -eq 0) {
+                throw "$($Type.Name) has no public instance constructors"
+            } elseif ($constructors.Count -eq 1) {
+                $cons = $constructors[0] |
+                Select-Object @{Name = 'Signature'; Expression = { $_.ToString() } },
+                @{Name = 'Params'; Expression = { $_.GetParameters() } }
+            } else {
+                $cons = $constructors |
+                Select-Object @{Name = 'Signature'; Expression = { $_.ToString() } },
+                @{Name = 'Params'; Expression = { $_.GetParameters() } } |
+                Out-GridView -PassThru
+                if (!$cons) {
+                    throw "Operation canceled."
                 }
             }
 
-            $params = $paramList -join ",`r`n"
+            $numParams = $cons.Params.Count
 
-            $body = "    New-Object $resType -ArgumentList $($valList -join ', ')"
-        } else {
-            $params = ''
+            if ($numParams -gt 0) {
+                $paramList = [List[string]]::new($numParams)
+                $valList = [List[string]]::new($numParams)
 
-            $body = "    New-Object $resType"
+                foreach ($p in $cons.Params) {
+                    [string] $paramType = $p.ParameterType
+                    $paramName = $p.Name.Substring(0, 1).ToUpper() + $p.Name.SubString(1)
+                    $valList.Add("`$$ParamName")
+
+                    [bool] $isBool = $p.ParameterType -eq [bool]
+                    if (!($NoSwitch) -and $isBool) {
+                        $paramType = 'switch'
+                        $hasSwitch = $true
+                    }
+                    if (!$hasSwitch -and $p.HasDefaultValue) {
+                        $defValue = $p.DefaultValue
+                        if ($isBool) {
+                            $defValue = if ($defValue) { '$true' } else { '$false' }
+                        } elseif ($null -eq $defValue) {
+                            $defValue = '$null'
+                        }
+                        $paramList.Add("        [$paramType] `$$($ParamName) = $defValue")
+                    } else {
+                        $paramList.Add("        [$paramType] `$$($ParamName)")
+                    }
+                }
+
+                $params = $paramList -join ",`r`n"
+                $body = "    New-Object $resType -ArgumentList $($valList -join ', ')"
+            } else {
+                $params = ''
+                $body = "    New-Object $resType"
+            }
         }
-    }
 
-    @"
+        @"
 function $Name {$Attribs
     param
     (
@@ -219,4 +231,5 @@ $params
 $body
 }
 "@
+    }
 }
