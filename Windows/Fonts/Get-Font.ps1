da5950de1fb8b91926915fb1a77d84f4105fc4fb
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-12-14 21:26:39 -0800

    Add support for wildcard search.

--- Windows/Fonts/Get-Font.ps1
+++ Windows/Fonts/Get-Font.ps1
@@ -4,6 +4,8 @@ function Get-Font {
         List installed fonts.
     .DESCRIPTION
         List installed fonts (system or user) based on information in registry.
+    .EXAMPLE
+        Get-Font -Type Machine -Name *Nerd* | Select-Object -First 3
     .NOTES
         Based on https://superuser.com/a/1534136/448598.
     #>
@@ -11,20 +13,26 @@ function Get-Font {
     param (
         [ValidateSet('User', 'Machine')]
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string]$Type
+        [string]$Type,
+
+        [SupportsWildcards()]
+        [string]$Name
     )
-    process {
+    begin {
         [bool] $isUser = $Type -eq 'User'
+        $predicate = $Name ? { $_.Name.SubString(0, $_.Name.LastIndexof(' ')) -like $Name } : { $_.Name -NotIn 'PSChildName', 'PSDrive', 'PSParentPath', 'PSPath', 'PSProvider' }
+    }
+    process {
         $drive = $isUser ? 'HKCU:' : 'HKLM:'
         Get-ItemProperty "$drive\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" |
             Get-Member -MemberType NoteProperty |
-            Where-Object Name -NotIn PSChildName, PSDrive, PSParentPath, PSPath, PSProvider |
+            Where-Object $predicate |
             Select-Object Name, @{
                 L = 'FileName'
                 E = {
                     $def = $_.Definition
                     $fileName = $def.SubString($def.IndexOf('=') + 1)
-                    $isUser ? $fileName : (Join-Path 'C:\Windows\Fonts' $fileName)
+                    $isUser ? $fileName : (Join-Path "$env:windir\Fonts" $fileName)
                 }
             }
     }
