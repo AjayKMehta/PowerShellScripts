e7f52dd732bde565b1b3ff245d198b0237dec5cc
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-12-17 08:48:10 -0800

    Allow Name to take values from pipeline.

--- Windows/Fonts/Get-Font.ps1
+++ Windows/Fonts/Get-Font.ps1
@@ -6,35 +6,38 @@ function Get-Font {
         List installed fonts (system or user) based on information in registry.
     .EXAMPLE
         Get-Font -Type Machine -Name *Nerd* | Select-Object -First 3
+    .EXAMPLE
+        'A*', 'T*' | Get-Font -Type Machine
     .NOTES
         Based on https://superuser.com/a/1534136/448598.
     #>
-    [CmdletBinding()]
+    [CmdletBinding(PositionalBinding = $false)]
     param (
         [ValidateSet('User', 'Machine')]
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $true)]
         [string]$Type,
 
         [SupportsWildcards()]
+        [Parameter(ValueFromPipeline = $true)]
         [string]$Name
     )
     begin {
         [bool] $isUser = $Type -eq 'User'
-        $drive = $isUser ? 'HKCU:' : 'HKLM:'
+        $regPath = Join-Path ($isUser ? 'HKCU:' : 'HKLM:') '\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
         $excluded = 'PSChildName', 'PSDrive', 'PSParentPath', 'PSPath', 'PSProvider'
+    }
+    process {
         $predicate = if ($Name) {
             {
                 # Font is <Font Family> <Style> (<FontType>).
-                $index = $_.Name.LastIndexof(' ')
+                $index = $_.Name.LastIndexOf(' ')
                 $check = ($index -eq -1) ? $_.Name : $_.Name.SubString(0, $index)
                 $_.Name -NotIn $excluded -and $check -like $Name
             }
         } else {
             { $_.Name -NotIn $excluded }
         }
-    }
-    process {
-        Get-ItemProperty "$drive\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" |
+        Get-ItemProperty $regPath |
             Get-Member -MemberType NoteProperty |
             Where-Object $predicate |
             Select-Object Name, @{
